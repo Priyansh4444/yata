4583d586e96751ae519406bbcb694b5daba81c55
Priyansh Shah <Priyanshpokemon@gmail.com>
2025-08-02 16:32:02 -0700

    feat: adding basic functionality for task-cards and task-lists

--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,1 +1,27 @@
-export default function TaskCard() {}
+export default function TaskCard({
+  header,
+  tags,
+}: {
+  header: string;
+  tags?: string[];
+}) {
+  return (
+    <div class="py-2 bg-gray-800 rounded-lg">
+      <div class="flex items-center justify-between px-4">
+        <h2 class="text-lg font-semibold text-white">{header}</h2>
+        {tags && tags.length > 0 && (
+          <div class="flex space-x-2">
+            {tags.map((tag) => (
+              <span class="px-2 py-1 text-xs font-medium text-gray-800 bg-gray-300 rounded-full">
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
+      </div>
+      <div class="px-4 py-2">
+        <p class="text-sm text-gray-400">Task description goes here...</p>
+      </div>
+    </div>
+  );
+}
